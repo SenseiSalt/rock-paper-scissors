9ccb0250808713de4905c11af22b7d068c872f57
SenseiSalt <thebeckster603@gmail.com>
2023-02-14 20:19:33 -0500

    Added winCondition function and a while loop that stops after 5 rounds

--- function.js
+++ function.js
@@ -8,103 +8,137 @@ const scissors = 3;
 const tie = 0;
 const win = 1;
 const lose = 2;
+let roundsPassed = 0;
 
-assignment(userInput);
 
-function assignment(users) {
+while(roundsPassed < 6) {
+
+
+    let userInput = prompt("Rock, Paper, or Scissors");
+    let computervalue = Math.ceil(Math.random() * 3);
+
+
+    assignment(userInput);
+
+    function assignment(users) {
     
     
-    switch(users) {
-        case "rock": userValue = rock;
-        break;
+        switch(users) {
+            case "rock": userValue = rock;
+            break;
 
-        case "paper": userValue = paper;
-        break;
+            case "paper": userValue = paper;
+            break;
 
-        case "scissors": userValue = scissors;
-        break;
+            case "scissors": userValue = scissors;
+            break;
 
-        default: userValue = 0;
-    }
+            default: userValue = 0;
+        }
     
-    logic(userValue, computervalue);
+        logic(userValue, computervalue);
 
 
 
-
-
-}
-
-
-function logic(uval, cval) {
-    switch(uval) {
-        case 1:
-            if(cval == 1) {
-                gameOutcome = tie;
-
-            }
-
-            else if(cval == 2) {
-                gameOutcome = lose;
-
-            }
-
-            else if(cval == 3) {
-                gameOutcome = win;
-
-
-            }
-        
-        break;
-
-
-        
-
-
-        case 2:
-            if(cval == 1) {
-                gameOutcome = win;
-
-            }
-
-            else if(cval == 2) {
-                gameOutcome = tie;
-
-            }
-
-            else if(cval == 3) {
-                gameOutcome = lose;
-
-
-            }
-        
-        break;
-
-
-
-        case 3:
-            if(cval == 1) {
-                gameOutcome = lose;
-
-            }
-
-            else if(cval == 2) {
-                gameOutcome = win;
-
-            }
-
-            else if(cval == 3) {
-                gameOutcome = tie;
-
-
-            }
-        
-        break;
 
 
     }
 
-    
+
+    function logic(uval, cval) {
+        switch(uval) {
+            case 1:
+                if(cval == 1) {
+                    gameOutcome = tie;
+
+                }
+
+                else if(cval == 2) {
+                    gameOutcome = lose;
+
+                }
+
+                else if(cval == 3) {
+                    gameOutcome = win;
+
+
+                }
+        
+            break;
+
+
+        
+
+
+            case 2:
+                if(cval == 1) {
+                    gameOutcome = win;
+
+                }
+
+                else if(cval == 2) {
+                    gameOutcome = tie;
+
+                }
+
+                else if(cval == 3) {
+                    gameOutcome = lose;
+
+
+                }
+        
+            break;
+
+
+
+            case 3:
+                if(cval == 1) {
+                    gameOutcome = lose;
+
+                }
+
+                else if(cval == 2) {
+                    gameOutcome = win;
+
+                }
+
+                else if(cval == 3) {
+                    gameOutcome = tie;
+
+
+                }
+        
+            break;
+
+
+        }
+
+
+        winCondition(gameOutcome);
+
+    }
+
+
+
+    function winCondition(winLoseOrTie) {
+        
+        
+        switch(winLoseOrTie) {
+            case 0: console.log("You Tie");
+            break;
+        
+            case 1: console.log("You Win");
+            break;
+
+            case 2: console.log("You Lose");
+            break;
+
+        }
+
+
+    }
+
+    roundsPassed++;
 
 
 }
