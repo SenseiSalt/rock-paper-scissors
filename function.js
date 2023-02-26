9915696e71af8d8c7277bbf5e605c6841df7abb7
SenseiSalt <thebeckster603@gmail.com>
2023-02-26 11:27:35 -0500

    finalized game, added round counter

--- function.js
+++ function.js
@@ -1,16 +1,17 @@
 userWins = 0;
 computerWins = 0;
-rounds = 0;
+
 imagesToChoose = document.querySelectorAll(".left-image");
 startGame = document.querySelector('.continue');
 blurredElements = document.querySelectorAll('#blurred');
 computerWinText = document.querySelector('#computer-wins');
 userWinsText = document.querySelector('#user-wins');
 startGame.addEventListener('click', playGame);
+roundsText = document.querySelector(".rounds-text");
 
 function playGame() {
-        
-    console.log("AAA");
+    rounds = 1;
+    roundsText.innerText = "Rounds: "
     userWins = 0;
     computerWins = 0;
     userWinsText.innerText = "User Wins:";
@@ -24,35 +25,23 @@ function playGame() {
 
 
 function beginGame(e) {
-    
-    console.log("AAA");
     id = e.target.getAttribute('id');
-    
 
     if (id == 'left-rock') {
         userChoice = 1;
         logic(userChoice);
-            
-
-        
-        
     }
 
     else if (id == 'left-paper') {
         userChoice = 2;
         logic(userChoice);
-
-
-
     }
 
 
     else if (id == 'left-scissors') {
         userChoice = 3;
         logic(userChoice);
-    }
-        
-
+    } 
 }
 
 
@@ -64,7 +53,6 @@ function logic(logicDecider) {
             if (computerChoice == 1) {
                 winOrLose = 1;
                 updateDOM(winOrLose);
-
                 }
 
             else if (computerChoice == 2) {
@@ -76,7 +64,6 @@ function logic(logicDecider) {
             else if (computerChoice == 3) {
                 winOrLose = 2;
                 updateDOM(winOrLose);
-
             }
 
         break;
@@ -85,22 +72,17 @@ function logic(logicDecider) {
             if (computerChoice == 1) {
                 winOrLose = 2;
                 updateDOM(winOrLose);
-
             }
            
             else if (computerChoice == 2) {
-
                 winOrLose = 1;
                 updateDOM(winOrLose);
             }
 
                     
             else if (computerChoice == 3) {
-
-
                 winOrLose = 0;
                 updateDOM(winOrLose);
-
             }
 
         break;
@@ -109,11 +91,9 @@ function logic(logicDecider) {
                     
         case 3:
             if (computerChoice == 1) {
-
                 winOrLose = 0;
                 updateDOM(winOrLose);
             }
-
 
             else if (computerChoice == 2) {
                 winOrLose = 2;
@@ -121,7 +101,6 @@ function logic(logicDecider) {
             }
                     
             else if (computerChoice == 3) {
-
                 winOrLose = 1;
                 updateDOM(winOrLose);
             }
@@ -133,51 +112,37 @@ function logic(logicDecider) {
 function updateDOM(whoWon) {
     userWinsText.innerText = "User Wins:";
     computerWinText.innerText = "Computer Wins:";
-            
+    roundsText.innerText = "Rounds: "
+    roundsText.innerText = roundsText.innerText + rounds;
+    rounds++;
             
     if (whoWon == 0) {
         computerWins++;
-
     }
 
     else if (whoWon == 1) {
-
         computerWins++;
         userWins++;
-
-
     }
 
     else if (whoWon == 2) {
         userWins++;
-
     }
 
     userWinsText.innerText = userWinsText.innerText + userWins;
     computerWinText.innerText = computerWinText.innerText + computerWins;
     endGame(userWins, computerWins);
-
-
-
 }
         
 function endGame(u, c) {
         
     if (u >= 5 || c >=5 ) {
-        rounds++;
+        
         imagesToChoose.forEach(image => image.style.display = 'none');
-
-                
-
-
         blurredElements.forEach(element => element.style.filter = 'blur(2px)');
         startGame.style.display = 'block';
-        startGame.innerText = "Play Again?";
-                
+        startGame.innerText = "Play Again?";   
     }
-
-
-
 }
 
     
